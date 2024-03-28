056206467b2dad662e6e8ab0348e06e464c47797
Dylan Price <the.dylan.price@gmail.com>
2024-03-28 16:12:32 -0700

    Work on settings that are synced to local storage.

--- src/options-ui.tsx
+++ src/options-ui.tsx
@@ -1,37 +1,86 @@
 import { render } from "preact";
-import { useState } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
+import browser from "webextension-polyfill";
 
-import Checkbox from "./components/Checkbox";
+const DEFAULT_DONATION_LINK = `https://secure.actblue.com/donate/alu-ont8election?express_lane=true&amount=`;
 
 function main() {
   render(<OptionsForm />, document.body);
 }
 
 function OptionsForm() {
-  const [value, setValue] = useState(10);
+  const {
+    loading: percentLoading,
+    value: percent,
+    save: savePercent,
+  } = useStorage("donationPercent", 10);
+  const {
+    loading: urlLoading,
+    value: url,
+    save: saveUrl,
+  } = useStorage("donationUrl", DEFAULT_DONATION_LINK);
+
+  const loading = percentLoading || urlLoading;
+
+  const labelStyle = {
+    display: "block",
+    marginBottom: "1em",
+  };
+
   return (
     <form onSubmit={this.onSubmit}>
-      <label style={{ display: "block" }}>
-        Donate <span> </span>
+      <label style={labelStyle}>
+        Donate{" "}
         <input
           type="number"
           min={0}
           max={100}
-          value={value}
+          value={percent}
           onChange={(event) =>
-            setValue(parseFloat((event.target as HTMLInputElement).value))
+            savePercent(parseFloat((event.target as HTMLInputElement).value))
           }
           style={{ width: "3em" }}
+          disabled={loading}
         />
         % of the shopping cart total
       </label>
-      <Checkbox
-        label="Test mode (load example.com instead of actblue)"
-        checked={true}
-        onClick={() => {}}
-      />
+      <label style={labelStyle}>
+        Dontaion URL:{" "}
+        <input
+          type="text"
+          value={url}
+          onInput={(event) => saveUrl((event.target as HTMLInputElement).value)}
+          style={{
+            width: "100%",
+          }}
+          disabled={loading}
+        />
+      </label>
     </form>
   );
+}
+
+function useStorage(key, default_ = undefined) {
+  const [value, setValue] = useState();
+
+  useEffect(() => {
+    const get = async () => {
+      const storedValue = await browser.storage.sync.get({ [key]: default_ });
+      setValue(storedValue[key]);
+    };
+    get();
+  }, []);
+  const save = async (newValue) => {
+    browser.storage.sync.set({
+      key: newValue,
+    });
+    setValue(newValue);
+  };
+  return {
+    loading: value === undefined,
+    value,
+    save: value === undefined ? undefined : save,
+  };
 }
 
 main();
