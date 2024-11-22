23fb05c0f80f5d709fd70287425da6ef5fc19d79
Dylan Price <the.dylan.price@gmail.com>
2024-11-21 21:17:00 -0800

    Start working on test.

--- src/extension/index.test.ts
+++ src/extension/index.test.ts
@@ -1,4 +1,10 @@
-import { queryByRole, findByRole } from "@testing-library/preact";
+import {
+  fireEvent,
+  findByRole,
+  getByRole,
+  queryByRole,
+  waitFor,
+} from "@testing-library/preact";
 
 import { getDonationPercent, getDonationUrl } from "../options";
 import { findButtons, isCheckoutPage, getDonationAmount } from "./find";
@@ -11,6 +17,22 @@ jest.mock("../options", () => ({
 }));
 jest.mock("./find");
 jest.spyOn(console, "log").mockImplementation(() => {});
+jest.spyOn(window, "open").mockImplementation(() => window);
+
+function buttonHtml(name = "placeYourOrder1") {
+  return `
+    <span id="submitOrderButtonId">
+      <span>
+        <input name="${name}" type="submit" />
+        <span>
+          <span>
+            Place your order
+          </span>
+        </span>
+      </span>
+    </span>
+  `;
+}
 
 describe("main", () => {
   test("logs when loaded", async () => {
@@ -18,18 +40,7 @@ describe("main", () => {
     expect(console.log).toHaveBeenCalledWith("alu-smile loaded");
   });
   test("does nothing when not checkout page", async () => {
-    document.body.innerHTML = `
-      <span id="submitOrderButtonId">
-        <span>
-          <input name="placeYourOrder1" />
-          <span>
-            <span>
-              Place your order
-            </span>
-          </span>
-        </span>
-      </span>
-    `;
+    document.body.innerHTML = buttonHtml();
 
     jest.mocked(getDonationPercent).mockResolvedValue(0.1);
     jest.mocked(getDonationAmount).mockReturnValue(10);
@@ -39,24 +50,9 @@ describe("main", () => {
     expect(queryByRole(document.body, "checkbox")).toBeNull();
   });
   test("attaches checkboxes to button on the checkout page", async () => {
-    document.body.innerHTML = `
-      <span id="submitOrderButtonId">
-        <span>
-          <input name="placeYourOrder1" />
-          <span>
-            <span>
-              Place your order
-            </span>
-          </span>
-        </span>
-      </span>
-    `;
+    document.body.innerHTML = buttonHtml();
 
-    const button = document
-      .getElementById("submitOrderButtonId")
-      .children.item(0)
-      .children.item(1)
-      .children.item(0);
+    const button = getByRole(document.body, "button");
     jest.mocked(getDonationPercent).mockResolvedValue(0.1);
     jest.mocked(getDonationAmount).mockReturnValue(10);
     jest.mocked(getDonationUrl).mockResolvedValue("http://example.com");
@@ -68,24 +64,9 @@ describe("main", () => {
     );
   });
   test("reattaches checkboxes when button is rerendered", async () => {
-    document.body.innerHTML = `
-      <span id="submitOrderButtonId">
-        <span>
-          <input name="placeYourOrder1" />
-          <span>
-            <span>
-              Place your order
-            </span>
-          </span>
-        </span>
-      </span>
-    `;
+    document.body.innerHTML = buttonHtml();
 
-    let button = document
-      .getElementById("submitOrderButtonId")
-      .children.item(0)
-      .children.item(1)
-      .children.item(0);
+    let button = getByRole(document.body, "button");
     jest.mocked(getDonationPercent).mockResolvedValue(0.1);
     jest.mocked(getDonationAmount).mockReturnValue(10);
     jest.mocked(getDonationUrl).mockResolvedValue("http://example.com");
@@ -96,27 +77,28 @@ describe("main", () => {
       /please donate \$10.00/,
     );
 
-    document.body.innerHTML = `
-      <span id="submitOrderButtonId">
-        <span>
-          <input name="placeYourOrder2" />
-          <span id="submitOrderButtonId-announce">
-            <span>
-              Place your order
-            </span>
-          </span>
-        </span>
-      </span>
-    `;
+    document.body.innerHTML = buttonHtml("placeYourOrder2");
 
-    button = document
-      .getElementById("submitOrderButtonId")
-      .children.item(0)
-      .children.item(1)
-      .children.item(0);
+    button = getByRole(document.body, "button");
     jest.mocked(findButtons).mockReturnValue([button]);
     expect(await findByRole(document.body, "checkbox")).toHaveAccessibleName(
       /please donate \$10.00/,
     );
+  });
+  test("only donates once even after multiple renders", async () => {
+    document.body.innerHTML = buttonHtml("placeYourOrder1");
+
+    const button = getByRole(document.body, "button");
+    jest.mocked(getDonationPercent).mockResolvedValue(0.1);
+    jest.mocked(getDonationAmount).mockReturnValue(10);
+    jest.mocked(getDonationUrl).mockResolvedValue("http://example.com");
+    jest.mocked(isCheckoutPage).mockReturnValue(true);
+    jest.mocked(findButtons).mockReturnValue([button]);
+    await main();
+    // TODO:
+    // 1. Fix test as-is
+    // 2. Append elements to the DOM to trigger re-renders, then test button only fires once.
+    fireEvent.click(button);
+    await waitFor(() => expect(window.open).toHaveBeenCalledTimes(1));
   });
 });
